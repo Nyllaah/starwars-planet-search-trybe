1ec7d9cca25315cc47df1b246c28d338664770d9
Nyllaah <nylla.g@hotmail.com>
2023-10-18 16:06:12 -0300

    feat: req 5

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import PlanetsProvider from '../context/planets-provider';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Verifica se o componente App.tsx renderiza corretamente', () => {
+  test('Existe o input de busca por nome', () => {
+    render(<PlanetsProvider children={<App />} />);
+
+    const nameIpt = screen.getByTestId('name-filter');
+    expect(nameIpt).toBeVisible();
+  });
+
+  test('Existe os inputs de busca numérica', () => {
+    render(<PlanetsProvider children={<App />} />);
+
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    const valueFilter = screen.getByTestId('value-filter');
+    const allOpts = screen.getAllByRole('option');
+
+    expect(columnFilter).toBeVisible();
+    expect(comparisonFilter).toBeVisible();
+    expect(valueFilter).toBeVisible();
+    expect(allOpts).toHaveLength(8);
+  });
+
+  test('Existe o botão de filtar a busca', () => {
+    render(<PlanetsProvider children={<App />} />);
+
+    const btn = screen.getByTestId('button-filter');
+    expect(btn).toBeVisible();
+    expect(btn).toHaveTextContent('Filter');
+  })
+})
